59305c5740dac58fb1bd2a6ef68458dc830974b1
Danyal Soomro <danyal.s.94@gmail.com>
2021-11-13 15:02:02 +0500

    Initial backend commit

---
+++ src/config.py
@@ -0,0 +1,30 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    BINANCE_API_SECRET = os.environ.get("BINANCE_API_SECRET", None)
+    BINANCE_API_KEY = os.environ.get("BINANCE_API_KEY", None)
+    ETHERSCAN_API_KEY = os.environ.get("ETHERSCAN_API_KEY", None)
+    BACKEND_API_PORT = os.environ.get("BACKEND_API_PORT", None)
+    BACKEND_API_IP = os.environ.get("BACKEND_API_IP", None)
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
